32d4be9bc473d770cfe4e0148e7bcc08a82b591c
daniocen777 <danielochoa@outlook.com>
2021-10-06 10:17:43 -0500

    Agregando alertas (simple y con input) en la pagina 'alert'

--- components/src/app/pages/alert/alert.page.ts
+++ components/src/app/pages/alert/alert.page.ts
@@ -1,15 +1,81 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AlertController, AlertInput } from '@ionic/angular';
+
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.page.html',
   styleUrls: ['./alert.page.scss'],
 })
 export class AlertPage implements OnInit {
+  valueInput: string = 'alert page';
 
-  constructor() { }
+  constructor(public alertController: AlertController) { }
 
   ngOnInit() {
+  }
+
+  /* Simple Alert */
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Okay',
+          handler: () => {
+            console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+
+  /* Alert input */
+  async presentAlertPrompt() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Prompt!',
+      inputs: [
+        {
+          name: 'name2',
+          type: 'text',
+          id: 'name2-id',
+          value: '',
+          placeholder: 'Nombre'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: (value) => {
+            this.valueInput = value.name2;
+            console.log(this.valueInput);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 
 }
