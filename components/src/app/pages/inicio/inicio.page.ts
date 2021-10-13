4817d60cb3dc1db8aa6c69f9de4605a47102b0ea
daniocen777 <danielochoa@outlook.com>
2021-10-13 16:18:30 -0500

    Creando página para componente 'ion-avatar'

--- components/src/app/pages/inicio/inicio.page.ts
+++ components/src/app/pages/inicio/inicio.page.ts
@@ -17,6 +17,7 @@ export class InicioPage implements OnInit {
   components: CustomComponent[] = [
     { icon: 'american-football', name: 'Action Sheet', redirecTo: '/action-sheet' },
     { icon: 'logo-apple-appstore', name: 'Alert', redirecTo: '/alert' },
+    { icon: 'logo-octocat', name: 'Avatar', redirecTo: '/avatar' },
   ];
 
   constructor() { }
